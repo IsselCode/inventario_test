83e0fea29f8a6cc6fd137aa35017b05166885579
issel <isselcode@gmail.com>
2025-06-25 14:27:58 -0600

    programación funcional con dartz para gestionar fallos y éxitos correctamente

--- lib/src/models/shared_model.dart
+++ lib/src/models/shared_model.dart
@@ -1,4 +1,6 @@
 import 'package:bcrypt/bcrypt.dart';
+import 'package:dartz/dartz.dart';
+import 'package:inventario_test/core/app/errors.dart';
 import 'package:inventario_test/src/clean_features/dtos/login_user_dto.dart';
 import 'package:inventario_test/src/clean_features/dtos/register_user_dto.dart';
 import 'package:inventario_test/src/clean_features/entities/user_entity.dart';
@@ -14,41 +16,45 @@ class SharedModel {
     required this.shared
   });
 
-  Future<UserEntity> registerUser(RegisterUserDTO rudto) async {
+  Future<Either<Failure, UserEntity>> registerUser(RegisterUserDTO rudto) async {
 
     try {
       await shared.setStringList(_USER, [rudto.name, rudto.username, rudto.hashedPassword]);
 
-      return UserEntity(name: rudto.name, username: rudto.username);
+      UserEntity user = UserEntity(name: rudto.name, username: rudto.username);
+
+      return Right(user);
 
     } catch (e) {
-      throw UnimplementedError();
+      return Left(AuthFailure(message: 'Algo sucedió al intentar registrar el usuario'));
     }
 
   }
 
-  UserEntity loginUser(LoginUserDTO ludt) {
+  Either<Failure, UserEntity> loginUser(LoginUserDTO ludt) {
 
     try {
 
       List<String>? tempList = shared.getStringList(_USER);
 
       if (tempList == null) {
-        throw Exception("Aun no hay ningun usuario registrado");
+        return Left(AuthFailure(message: "Aun no hay ningun usuario registrado"));
       }
 
       bool samePassword = BCrypt.checkpw(ludt.password, tempList[2]);
       bool sameUsername = ludt.username == tempList[1];
 
       if (!samePassword || !sameUsername) {
-        throw Exception("Las credenciales no son correctas");
+        return Left(AuthFailure(message: "Las credenciales no son correctas"));
       }
 
-      return UserEntity(name: tempList[0], username: tempList[1]);
+      UserEntity user = UserEntity(name: tempList[0], username: tempList[1]);
+
+      return Right(user);
 
     } catch (e) {
 
-      throw UnimplementedError();
+      return Left(AuthFailure(message: 'Algo sucedió al intentar registrar el usuario'));
 
     }
 
