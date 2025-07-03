18dccc974e1bfd8a06d28d843fd6e1bcfcb26d87
issel <isselcode@gmail.com>
2025-07-02 19:12:07 -0600

    Agregando interactividad al PageView

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -4,8 +4,28 @@ import 'package:inventario_test/src/views/pages/inventory_page.dart';
 import 'package:inventario_test/src/views/pages/profile_page.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class HomeView extends StatefulWidget {
+
+  const HomeView({
+    super.key
+  });
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+
+  final PageController pageController = PageController(initialPage: 0);
+
+  changePage(int index) async {
+    await pageController.animateToPage(
+      index,
+      duration: Duration(milliseconds: 350),
+      curve: Curves.linear
+    );
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +33,8 @@ class HomeView extends StatelessWidget {
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        currentIndex: pageController.page!.toInt(),
+        onTap: changePage,
         items: [
           BottomNavigationBarItem(icon: Icon(Symbols.box), label: "",),
           BottomNavigationBarItem(icon: Icon(Symbols.receipt_long), label: "",),
@@ -20,11 +42,12 @@ class HomeView extends StatelessWidget {
         ]
       ),
       body: PageView(
+        controller: pageController,
         physics: NeverScrollableScrollPhysics(),
         children: [
-          ProfilePage(),
           InventoryPage(),
           HistoryPage(),
+          ProfilePage(),
         ],
       ),
     );
