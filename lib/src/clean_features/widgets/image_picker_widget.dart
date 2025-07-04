2176d2256b6352d219d0e64ec22dbba618122dc6
issel <isselcode@gmail.com>
2025-07-04 11:59:36 -0600

    Selector de imágenes con image Picker

---
+++ lib/src/clean_features/widgets/image_picker_widget.dart
@@ -0,0 +1,110 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImagePickerWidget extends StatefulWidget {
+
+  final Function(Uint8List? image) onChanged;
+  final Uint8List? initialImage;
+
+  const ImagePickerWidget({
+    super.key,
+    required this.onChanged,
+    this.initialImage
+  });
+
+  @override
+  State<ImagePickerWidget> createState() => _ImagePickerWidgetState();
+}
+
+class _ImagePickerWidgetState extends State<ImagePickerWidget> {
+
+  Uint8List? showImage;
+
+  @override
+  void initState() {
+    super.initState();
+    showImage = widget.initialImage;
+  }
+
+  Future<void> selectImage() async {
+    // Creamos instancia de ImagePicker
+    ImagePicker imagePicker = ImagePicker();
+
+    // Configuramos para obtener imagen desde la galería
+    XFile? pickImage = await imagePicker.pickImage(source: ImageSource.gallery);
+
+    // Comprobamos que se haya seleccionado una imagen
+    if (pickImage == null) {
+      return;
+    }
+
+    // Creamos instancia del archivo
+    File file = File(pickImage.path);
+
+    // Convertimos el archivo a bytes
+    Uint8List image = await file.readAsBytes();
+
+    // Asignamos la imagen
+    showImage = image;
+
+    // Repintamos el widget
+    setState(() {});
+
+    // Enviamos la imagen al widget padre
+    widget.onChanged(image);
+  }
+
+  void clearImage() {
+    showImage = null;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        // Contenedor de imagen
+        InkWell(
+          onTap: selectImage,
+          borderRadius: BorderRadius.circular(24),
+          child: Container(
+            margin: EdgeInsets.all(3),
+            width: 250,
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: Center(
+              child: showImage == null
+                ? Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Image.asset("assets/no_image.jpg")
+                      ),
+                      const SizedBox(height: 10,),
+                      Text("Selecciona una imagen")
+                    ],
+                  ),
+                )
+                : Image.memory(showImage!, height: 220, width: 220,)
+            ),
+          ),
+        ),
+        // Botón para eliminar imagen
+        Positioned(
+          right: 0,
+          child: CloseButton(
+            color: Colors.red,
+            onPressed: clearImage,
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/src/views/add_new_product_view.dart
+++ lib/src/views/add_new_product_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:inventario_test/src/clean_features/widgets/image_picker_widget.dart';
 
 class AddNewProductView extends StatelessWidget {
   const AddNewProductView({super.key});
@@ -7,6 +8,24 @@ class AddNewProductView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              children: [
+                // Imagen
+                ImagePickerWidget(
+                  onChanged: (image) {
+                    print(image);
+                  },
+                ),
+                // TextFields
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
